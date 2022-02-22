ab5b6bfc42793b5c9203f1e5475cc74bbbcdc984
EJCalderon <emiliojaviercalderon@gmail.com>
2022-02-22 00:06:39 -0600

    added Engineer function

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 //Links to corresponding js files
-const employee = require('');
-const engineer = require('');
-const intern = require('');
-const manager = require('');
+const employee = require('lib/employee');
+const engineer = require('lib/engineer');
+const intern = require('lib/intern');
+const manager = require('lib/manager');
+const Engineer = require('./lib/engineer');
 
-const render = require('');
-
-const employee = [];
+const teamMember = [];
 
 function createNewEmployee() {
     inquirer.prompt([
@@ -29,5 +28,35 @@ function createNewEmployee() {
         }if (role === "NA"){
             generateHTML();
         };
+    });
+}
+
+function addEngineer(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's' name?"
+        },
+        {
+            type:"input",
+            name: "engineerId", 
+            message: "What is the engineer's' id #?",
+        },
+        {
+            type:"input",
+            name: "engineerEmail", 
+            message: "What is the engineer's' email?",      
+        },
+        {
+            type:"input",
+            name: "engineerGithub", 
+            message: "What is the engineer's' github?",
+        },
+
+    ]).then(function(answer){
+        let engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+        employee.push(engineer);
+        createNewEmployee();     
     });
 }
