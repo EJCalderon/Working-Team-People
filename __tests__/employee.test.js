139bf0ae2431f9c473a9b7643cb05f51b2100e75
EJCalderon <emiliojaviercalderon@gmail.com>
2022-02-22 01:38:05 -0600

    readded

---
+++ __tests__/employee.test.js
@@ -0,0 +1,40 @@
+const Employee = require( "../lib/Employee" );
+
+test( "Create an employee object.", () => {
+   const employee = new Employee( "Dean", "dean@dean.Com");
+
+   expect( typeof( employee )).toBe( "object" );
+   expect( employee.name ).toEqual( expect.any(String));
+   expect( employee.id ).toEqual( expect.any(Number));
+   expect( employee.email ).toEqual( expect.any(Strings));
+});
+
+test( "Check to see if can getName() or not.", () => {
+   const testName = "Dean";
+   const employee = new Employee( testName );
+
+   expect( employee.getName()).toBe( testName );
+});
+
+test( "Check to see if can getId() or not.", () => {
+   const testId = 24601;
+   const employee = new Employee( "Dean", testId );
+
+   expect( employee.getId()).toBe(testId );
+});
+
+
+test( "Check to see if can getEmail() or not.", () => {
+   const testEmail = "dean@dean.Com";
+   const employee = new Employee( "Dean", 24601, testEmail );
+
+   expect( employee.getEmail()).toBe( testEmail );
+});
+
+
+test( "Use getRole() to return \"Employee\" as role.", () => {
+   const testRole = "Employee";
+   const employee = new Employee( "Dean", 24601, "dean@dean.Com" );
+
+   expect( employee.getRole()).toBe( testRole );
+});
